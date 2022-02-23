8b9f04b511237eb28aadbd3a8e450590ec9ab19a
fakhrinail <fakhrinailwbw@gmail.com>
2022-02-23 17:58:25 +0700

    move init shader to modular function

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 import getColorDataFromInput from "./getColorData.js";
+import initShaders from "./initShaders.js";
 
 const setColorData = () => {
   const colorData = getColorDataFromInput();
@@ -30,23 +31,22 @@ if (!gl) {
   throw new Error("WebGL not supported");
 }
 
-const vertexData = [
-  0,
-  1,
-  0, // V1.position
-  1,
-  -1,
-  0, // V2.position
-  -1,
-  -1,
-  0, // V3.position
-];
-
 // const vertexData = [
-//   0, 0, 1, 1, -0.4999999999999998, -0.4999999999999998, 0, 0,
-//   -0.4999999999999998, -0.4999999999999998, -0.5000000000000004,
-//   -0.5000000000000004, 0, 0, -0.5000000000000004, -0.5000000000000004, 1, 1,
+//   0,
+//   1,
+//   0, // V1.position
+//   1,
+//   -1,
+//   0, // V2.position
+//   -1,
+//   -1,
+//   0, // V3.position
 // ];
+
+const vertexData = [
+  1, 0, -0.4999999999999998, 0.8660254037844387, -0.5000000000000004,
+  -0.8660254037844385,
+];
 
 const colorData = getColorDataFromInput();
 
@@ -58,42 +58,8 @@ const colorBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(
-  vertexShader,
-  `
-precision mediump float;
-attribute vec3 position;
-attribute vec3 color;
-varying vec3 vColor;
-uniform mat4 matrix;
-void main() {
-    vColor = color;
-    gl_Position = matrix * vec4(position, 1);
-}
-`
-);
-gl.compileShader(vertexShader);
-
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(
-  fragmentShader,
-  `
-precision mediump float;
-varying vec3 vColor;
-void main() {
-    gl_FragColor = vec4(vColor, 1);
-}
-`
-);
-gl.compileShader(fragmentShader);
-console.log(gl.getShaderInfoLog(fragmentShader));
-
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
+const program = initShaders(gl, "vertex-shader", "fragment-shader");
+gl.useProgram(program);
 
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
